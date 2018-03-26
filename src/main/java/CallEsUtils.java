108b007fce91afa1cf073cb6a0c403659dab00c3
cxq <caoxiaoqiang@xiaomi.com>
2018-03-26 20:31:54 +0800

    java call defined package

---
+++ src/main/java/CallEsUtils.java
@@ -0,0 +1,11 @@
+import EsUtils.EsOperation;
+
+/**
+ * Created by cxq on 2018/3/26.
+ */
+public class CallEsUtils {
+    public static void main(String[] args) {
+        EsOperation esu = new EsOperation();
+        esu.run();
+    }
+}
--- src/main/java/EsUtils/EsOperation.java
+++ src/main/java/EsUtils/EsOperation.java
@@ -4,4 +4,7 @@ package EsUtils;
  * Created by cxq on 2017/6/26.
  */
 public class EsOperation {
+    public void run(){
+        System.out.println("hello world");
+    }
 }
