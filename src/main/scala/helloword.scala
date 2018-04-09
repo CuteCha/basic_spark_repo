94027e8d791509ba6fa268ed2cb213e34a8f0c71
cxq <caoxiaoqiang@xiaomi.com>
2018-04-09 19:27:15 +0800

    array

--- src/main/scala/helloword.scala
+++ src/main/scala/helloword.scala
@@ -4,6 +4,14 @@
 object helloword {
   def main(args: Array[String]): Unit = {
     println("hello world!~~scala~~")
+
+    val a = new Array[Array[String]](3)
+    a.update(0, Array("Runoob", "Baidu", "Google"))
+    a.update(1, Array("A", "B", "C"))
+    a.update(2, Array("D", "E", "F"))
+    for (s <- a(0))
+      println(s)
+    println(a(0))
   }
 
 }
