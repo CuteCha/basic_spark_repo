c7b6a3acb7eff1d5980bd4b89eaff17db9188a0b
cxq <caoxiaoqiang@xiaomi.com>
2018-04-25 10:54:52 +0800

    add clusterSettings to resolve error NoNodeAvailableException

--- src/main/java/EsUtils/EsCon.java
+++ src/main/java/EsUtils/EsCon.java
@@ -16,8 +16,12 @@ public class EsCon {
     private static int port = 9300;
 
     public static void main(String[] args) throws Exception {
+        Settings clusterSettings = Settings.builder()
+                .put("cluster.name", "cxq-application")
+                .put("client.transport.sniff", true)
+                .build();
         @SuppressWarnings({"resource", "unchecked"})
-        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+        TransportClient client = new PreBuiltTransportClient(clusterSettings)
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
         System.out.println(client);
         GetResponse response=client.prepareGet("blog", "article", "1").get();
