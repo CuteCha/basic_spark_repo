89c6377f4527e4fd34fc6c854fb637ce8dc0fed7
cxq <caoxiaoqiang@xiaomi.com>
2018-04-28 14:33:03 +0800

    es operation

--- src/main/java/EsUtils/TestCon.java
+++ src/main/java/EsUtils/TestCon.java
@@ -1,7 +1,33 @@
 package EsUtils;
 
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+
 /**
  * Created by cxq on 2018/4/26.
  */
 public class TestCon {
+    private static String host="10.231.55.250";
+    private static int port=9300;
+    public static final String CLUSTER_NAME="cxq-application";
+
+    private static Settings clusterSettings = Settings.builder()
+                                                    .put("cluster.name", CLUSTER_NAME)
+                                                    .put("client.transport.sniff", true)
+                                                    .build();
+
+    public static void main(String[] args) throws Exception{
+        @SuppressWarnings({ "resource", "unchecked" })
+        TransportClient client = new PreBuiltTransportClient(clusterSettings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host),port));
+        System.out.println(client);
+        GetResponse response=client.prepareGet("blog", "article", "1").get();
+        System.out.println(response.getSourceAsString());
+        client.close();
+    }
 }
--- src/main/java/strUtils/StrTest.java
+++ src/main/java/strUtils/StrTest.java
@@ -1,7 +1,24 @@
 package strUtils;
 
+import org.junit.Test;
+
 /**
  * Created by cxq on 2018/4/26.
  */
 public class StrTest {
+    @Test
+    public void testTrim() throws Exception{
+        String str1 = "abc   ";
+        String str2 = "  abc";
+        String str3 = "a bc";
+        String str4 = " a bc ";
+        System.out.println(str1+"|\n"+str1.trim()+"|");
+        System.out.println("------------");
+        System.out.println(str2+"|\n"+str2.trim()+"|");
+        System.out.println("------------");
+        System.out.println(str3+"|\n"+str3.trim()+"|");
+        System.out.println("------------");
+        System.out.println(str4+"|\n"+str4.trim()+"|");
+        System.out.println("------------");
+    }
 }
