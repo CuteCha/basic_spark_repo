c2e264b7c53489017d4a0951dfecaddc3820b14a
cxq <caoxiaoqiang@xiaomi.com>
2018-08-25 10:10:40 +0800

    code reformat

--- src/main/java/com/cxq/concurrent/ForkJoinDemo.java
+++ src/main/java/com/cxq/concurrent/ForkJoinDemo.java
@@ -8,11 +8,11 @@ import java.util.concurrent.RecursiveTask;
  * Created by cxq on 2018/6/4.
  */
 public class ForkJoinDemo {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        Future<Long> res = forkJoinPool.submit(new ForkJoinDemoM(1,1000000));
+        Future<Long> res = forkJoinPool.submit(new ForkJoinDemoM(1, 1000000));
 
-        System.out.println("res: "+ res);
+        System.out.println("res: " + res);
         forkJoinPool.shutdown();
     }
 }
@@ -35,18 +35,18 @@ class ForkJoinDemoM extends RecursiveTask<Long> {
             for (int i = begin; i <= end; i++) {
                 sum += i;
             }
-        }else {
-            int mid=(begin+end)/2;
-            ForkJoinDemoM leftM = new ForkJoinDemoM(begin,mid);
+        } else {
+            int mid = (begin + end) / 2;
+            ForkJoinDemoM leftM = new ForkJoinDemoM(begin, mid);
             leftM.fork();
-            ForkJoinDemoM rightM = new ForkJoinDemoM(mid+1, end);
+            ForkJoinDemoM rightM = new ForkJoinDemoM(mid + 1, end);
             rightM.fork();
 
-            Long leftRes=leftM.join();
-            System.out.println(begin+"~"+mid+": "+ leftRes);
+            Long leftRes = leftM.join();
+            System.out.println(begin + "~" + mid + ": " + leftRes);
             Long rightRes = rightM.join();
-            System.out.println((mid+1)+"~"+end+": "+ rightRes);
-            sum = leftRes+rightRes;
+            System.out.println((mid + 1) + "~" + end + ": " + rightRes);
+            sum = leftRes + rightRes;
 
         }
         return sum;
