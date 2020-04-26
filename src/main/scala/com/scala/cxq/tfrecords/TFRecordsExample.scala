c877f8f471d509d38805fe55fef18c49edb8f0c8
cxq <caoxiaoqiang@xiaomi.com>
2020-04-26 15:35:37 +0800

    spark tfrecords

---
+++ src/main/scala/com/scala/cxq/tfrecords/TFRecordsExample.scala
@@ -0,0 +1,18 @@
+package com.scala.cxq.tfrecords
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import collection.JavaConversions._
+import org.apache.log4j.{ Level, Logger }
+import org.apache.spark.SparkConf
+
+import org.apache.spark.sql.{ DataFrame, Row }
+import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.apache.spark.sql.types._
+
+
+/**
+  * Created by cxq on 2020-01-28 15:59
+  */
+object TFRecordsExample {
+
+}
